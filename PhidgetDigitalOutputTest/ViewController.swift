02ddc8078c6f54dc98dc1e781e2d3098c2767420
Your Name <you@example.com>
2019-03-19 14:10:53 -0600

    Initial Commit

---
+++ PhidgetDigitalOutputTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhidgetDigitalOutputTest
+//
+//  Created by Gregory Paul Sim on 2019-03-19.
+//  Copyright © 2019 Gregory Paul Sim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
