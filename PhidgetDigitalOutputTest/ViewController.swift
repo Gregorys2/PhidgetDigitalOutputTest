f9913a03f967d5ba9f4b83505e8b7445b61add47
Your Name <you@example.com>
2019-03-21 14:50:19 -0600

    Initial commit, nothing works help.

--- PhidgetDigitalOutputTest/ViewController.swift
+++ PhidgetDigitalOutputTest/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import Phidget22Swift
 
 class ViewController: UIViewController {
+    let led = DigitalOutput()
+    let button = DigitalInput()
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do{
+            try Net.enableServerDiscovery(serverType: .deviceRemote)
+            
+            try led.setDeviceSerialNumber(527868)
+            try led.setHubPort(3)
+            try led.setIsHubPortDevice(true)
+            
+            try button.setDeviceSerialNumber(527868)
+            try button.setHubPort(1)
+            try button.setIsHubPortDevice(true)
+            
+            let _ = led.attach.addHandler(attach_handler)
+            let _ = button.attach.addHandler(attach_handler)
+            
+            let _ = button.stateChange.addHandler(state_change)
+            
+         
+            try button.open()
+            try led.open()
+            
+        } catch let err as PhidgetError {
+            print(err)
+        } catch {
+            print(error)
+        }
     }
-
-
+    
+    
+    func attach_handler(sender: Phidget){
+        do{
+            if(try sender.getHubPort() == 3){
+                print("LED Attached")
+            }
+            else{
+                print("Button Attached")
+            }
+        } catch let err as PhidgetError{
+            print("Phidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
+    
+    
+    func state_change(sender:DigitalInput, state:Bool){
+        do{
+            if(state == true){
+                print("Button Pressed")
+                try led.setState(true)
+            }
+            else{
+                print("Button Not Pressed")
+                try led.setState(false)
+            }
+        } catch let err as PhidgetError{
+            print("PPPhidget Error " + err.description)
+        } catch{
+            //catch other errors here
+        }
+    }
+    
 }
-
---
+++ Pods/Target Support Files/Pods-PhidgetDigitalOutputTest/Pods-PhidgetDigitalOutputTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhidgetDigitalOutputTest : NSObject
+@end
+@implementation PodsDummy_Pods_PhidgetDigitalOutputTest
+@end
